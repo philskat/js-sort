651f5486b37a25369c05a9bfcab908f80a79fd73
philskat <skatphil@gmail.com>
2021-09-03 00:44:19 +0200

    Fixed capitalization and bump version

--- src/algorithms/insertionSort.ts
+++ src/algorithms/insertionSort.ts
@@ -2,7 +2,7 @@
  * Insert an element into an sorted array
  *
  * @param arr Sorted array the element should be injected into
- * @param val value that should be inserted
+ * @param val Value that should be inserted
  * @returns Sorted array with the new value
  */
 export function insertSorted(arr: number[], val: number): number[] {
